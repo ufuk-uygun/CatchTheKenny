04c9a4e6c65c064e07ee477812cae45084b503f4
ufuk <ufukuygun@gmail.com>
2022-08-14 19:30:42 +0300

    finish

--- CatchTheKenny/ViewController.swift
+++ CatchTheKenny/ViewController.swift
@@ -9,14 +9,111 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet var starButtons: [UIButton]!
+    var score:Int=0
+    var highScore:Int=0
+    var positionIndex:Int=0
     
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var highScoreLabel: UILabel!
+    
+    @IBOutlet var starButtons: [UIButton]!
+    @IBAction func starButtonTapped(_ sender: UIButton) {
+       
+        score = score + 1
+        
+        scoreLabel.text="Score:\(score)"
+        
+        saveHighScore()
+                             
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        //read highScore from userDefaults
+        let hs =  UserDefaults.standard.object(forKey: "highScore")
+        
+        if let hs = hs as? Int{
+            highScore=hs
+            refreshHighScore()
+             
+        }
+        
+        hiddenAllItems()
+       
+        Timer.scheduledTimer(timeInterval: 0.8, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+        
     }
+    
+    
+    @objc func updateTime() {
+        showItems()
+    }
+    
+    
+    func hiddenAllItems(){
+        
+        for button in starButtons{
+               
+            button.isHidden=true
+        }
+    }
+    
+    func showItems(){
+        
+        hiddenAllItems()
+        
+        let randomInt = Int.random(in: 0..<16)
+        
+        for button in starButtons{
+            
+            if (button.tag == randomInt)
+            {
+                button.isHidden=false
+            }
+                
+        }
+              
+    }
+    
+    
 
+    @IBAction func restHighScoreTapped(_ sender: Any) {
+   
+        
+    let hs = UserDefaults.standard.object(forKey: "highScore")
+    
+       if hs is Int? {
+           UserDefaults.standard.removeObject(forKey: "highScore")
+           
+           score = 0
+           highScore=0
+           
+           refreshHighScore()
+       }
+             
+    }
+    
+    func saveHighScore(){
+        
+        if (score>highScore){
+            UserDefaults.standard.set(score, forKey: "highScore")
+            
+            highScore = score
+            
+            refreshHighScore()
+            
+        }
+        
+    }
+    
+    
+    func refreshHighScore() {
+        
+        scoreLabel.text="Score:\(score)"
+        highScoreLabel.text = "HS:\(highScore)"
+    }
+     
 
 }
 
