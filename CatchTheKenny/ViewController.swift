c93a00aa9899ec3c478daddd9c023532103ada55
ufuk <ufukuygun@gmail.com>
2022-08-14 17:04:38 +0300

    yıldızlar eklendi

--- CatchTheKenny/ViewController.swift
+++ CatchTheKenny/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var starButtons: [UIButton]!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
